d38b785d89fa1eaac14ad7b460861b4e3740a735
Rosana <rosanartec@gmail.com>
2021-02-28 23:17:13 +0000

    added npm packages

--- index.js
+++ index.js
@@ -1,9 +1,34 @@
-var express = require("express");
-var app = express();
+process.stdout.write('\x1B[2J\x1B[0f') // Clear terminal screen
+require('dotenv').config()
+
+const express = require('express')
+const cors = require('cors')
+const mongoose = require('mongoose')
+const morgan = require('morgan')
+const path = require('path')
+
+mongoose.connect(process.env.MONGO_URL,
+  {
+    dbName: process.env.MONGO_DB || 'test',
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useUnifiedTopology: true
+  }, err => {
+    if (err) { throw new Error(err) }
+    console.info('💾 Connected to Mongo Database \n')
+  })
+
+// ADDING MIDDLEWARES & ROUTER
+const app = express()
+  .use(cors())
+  .use(morgan('combined'))
+  .use(express.json())
+  .use('/api', require('./router/index.js'))
+
 
 app.listen(3000, function () {
   
   console.info('>'.repeat(40))
   console.info('💻  Rosana Server Live')
-  console.info(`📡  PORT: http://localhost:300`)
+  console.info(`📡  PORT: http://localhost:3000`)
   console.info('>'.repeat(40) + '\n')})

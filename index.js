2ea274099537e8fafbc374cb6fdee0663ae91fa7
Rosana <rosanartec@gmail.com>
2021-02-28 21:50:35 +0000

    test

---
+++ index.js
@@ -0,0 +1,9 @@
+var express = require("express");
+var app = express();
+
+app.listen(3000, function () {
+  
+  console.info('>'.repeat(40))
+  console.info('💻  Rosana Server Live')
+  console.info(`📡  PORT: http://localhost:300`)
+  console.info('>'.repeat(40) + '\n')})

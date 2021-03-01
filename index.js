b2727f8ae7055570710b0862df1cf417c90f8f7f
Rosana <rosanartec@gmail.com>
2021-03-01 16:57:43 +0000

    all in folder api

--- index.js
+++ index.js
@@ -1,34 +1,38 @@
-process.stdout.write('\x1B[2J\x1B[0f') // Clear terminal screen
-require('dotenv').config()
+process.stdout.write("\x1B[2J\x1B[0f"); // Clear terminal screen
+require("dotenv").config();
 
-const express = require('express')
-const cors = require('cors')
-const mongoose = require('mongoose')
-const morgan = require('morgan')
-const path = require('path')
+const express = require("express");
+const cors = require("cors");
+const mongoose = require("mongoose");
+const morgan = require("morgan");
+const path = require("path");
 
-mongoose.connect(process.env.MONGO_URL,
+mongoose.connect(
+  process.env.MONGO_URL,
   {
-    dbName: process.env.MONGO_DB || 'test',
+    dbName: process.env.MONGO_DB || "test",
     useNewUrlParser: true,
     useCreateIndex: true,
-    useUnifiedTopology: true
-  }, err => {
-    if (err) { throw new Error(err) }
-    console.info('💾 Connected to Mongo Database \n')
-  })
+    useUnifiedTopology: true,
+  },
+  (err) => {
+    if (err) {
+      throw new Error(err);
+    }
+    console.info("💾 Connected to Mongo Database \n");
+  }
+);
 
 // ADDING MIDDLEWARES & ROUTER
 const app = express()
   .use(cors())
-  .use(morgan('combined'))
+  .use(morgan("combined"))
   .use(express.json())
-  .use('/api', require('./router/index.js'))
-
+  .use("/api", require("./api/router/index.js"));
 
 app.listen(3000, function () {
-  
-  console.info('>'.repeat(40))
-  console.info('💻  Rosana Server Live')
-  console.info(`📡  PORT: http://localhost:3000`)
-  console.info('>'.repeat(40) + '\n')})
+  console.info(">".repeat(40));
+  console.info("💻  Rosana Server Live");
+  console.info(`📡  PORT: http://localhost:3000`);
+  console.info(">".repeat(40) + "\n");
+});

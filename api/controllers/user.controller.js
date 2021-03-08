a55e388acf319fa9a6516caeb98bcbf386c870ce
Fer Martin <fer@reboot.academy>
2021-03-08 21:00:02 +0000

    userController clean up

--- api/controllers/user.controller.js
+++ api/controllers/user.controller.js
@@ -1,12 +1,5 @@
-const userModel = require('../models/user.model')
-const { handleError } = require("../utils")
-
-function getProfile(req, res) {
-  console.log(res.locals.userId)
-  userModel
-    .findById(res.locals.userId)
-    .then(response => res.json(response))
-    .catch((err) => handleError(err, res))
+function getProfile (req, res) {
+  res.json(res.locals.user)
 }
 
 module.exports = {
